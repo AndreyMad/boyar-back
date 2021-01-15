76b746568f9bb61559e78bc2eac687ed881f30cb
andreymad <andrey.mad.87@gmail.com>
2021-01-15 19:02:09 +0200

    add editing

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 const db = require("./postgres/index");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const path = require('path');
 
 const jsonParser = bodyParser.json();
 
@@ -18,7 +17,8 @@ app.listen(port, () => {
   console.log(`Example app aa:${port}`);
 });
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../boyar-front", "build", "index.html"));
+  res.send('hello world')
+  // res.sendFile(path.join(__dirname, "../boyar-front", "build", "index.html"));
 });
 
 app.post("/api/getDots",jsonParser, async (req, res) => {
@@ -41,6 +41,15 @@ app.post("/api/deleteDot",jsonParser, async (req, res) => {
 app.post("/api/addDot",jsonParser, async (req, res) => {
   // console.log(req.body.data);
    const dataResp = await db.addDot(req.body.data);
+   if (dataResp.error) {
+     return res.status(200).send(dataResp);
+   }
+    return res.status(200).send(dataResp);
+});
+
+app.post("/api/editDot",jsonParser, async (req, res) => {
+  // console.log(req.body.data);
+   const dataResp = await db.editDot(req.body.dot);
    if (dataResp.error) {
      return res.status(200).send(dataResp);
    }
--- postgres/index.js
+++ postgres/index.js
@@ -1,41 +1,57 @@
 const pg = require("pg");
 
-  var conString = "postgres://xjqyfann:VsFPK5ncA9t_z-qWxK0bFnhm7-Q5Yp8m@dumbo.db.elephantsql.com:5432/xjqyfann" //Can be found in the Details page
-  var client = new pg.Client(conString);
-  client.connect(err => {
-    if (err) {
-      return console.error("could not connect to postgres", err);
-    }
-    return console.log("postgree connected");
+var conString =
+  "postgres://xjqyfann:VsFPK5ncA9t_z-qWxK0bFnhm7-Q5Yp8m@dumbo.db.elephantsql.com:5432/xjqyfann"; //Can be found in the Details page
+var client = new pg.Client(conString);
+client.connect((err) => {
+  if (err) {
+    return console.error("could not connect to postgres", err);
+  }
+  return console.log("postgree connected");
+});
+
+const getDots = () => {
+  return client.query("SELECT * FROM yandexboyardots").then((res) => {
+    return res.rows;
   });
-
-
- const getDots=  ()=>{
-      return  client.query('SELECT * FROM yandexboyardots').then(res=>{
-          return res.rows
-      })}
-const deleteDot=  (id)=>{
-        return  client.query(`Delete from yandexboyardots where id = '${id}'  `).then(res=>{
-        if( res.rowCount===0){
-          return 'Id not in database'
-        }
-        return getDots()
-       })
+};
+const deleteDot = (id) => {
+  return client
+    .query(`Delete from yandexboyardots where id = '${id}'  `)
+    .then((res) => {
+      if (res.rowCount === 0) {
+        return "Id not in database";
       }
-const addDot= (data)=>{
+      return getDots();
+    });
+};
 
-const query = `insert into yandexboyardots (latitude, longtitude, name, description, id) VALUES ('${data.latitude}', '${data.longtitude}', '${data.name}', '${data.description}', '${data.id}')`
-return  client.query(query)
-  .then(res=>{
-    if( res.rowCount===0){
-      return 'Не добавлено'
+const addDot = (data) => {
+  const query = `insert into yandexboyardots (latitude, longtitude, name, description, id) VALUES ('${data.latitude}', '${data.longtitude}', '${data.name}', '${data.description}', '${data.id}')`;
+  return client.query(query).then((res) => {
+    if (res.rowCount === 0) {
+      return "Не добавлено";
     }
-    return getDots()
-  })
-
-
-}
-
-  module.exports.getDots=getDots
-  module.exports.deleteDot=deleteDot
-  module.exports.addDot=addDot
+    return getDots();
+  });
+};
+const editDot = ({ name, latitude, longtitude, description, id }) => {
+  const query = `UPDATE yandexboyardots SET
+      latitude = '${latitude}',
+      longtitude = '${longtitude}',
+      name = '${name}',
+      description ='${description}'
+  WHERE id = '${id}'
+   `;
+  return  client.query(query)
+    .then(res=>{
+      if( res.rowCount===0){
+        return 'Не изменено'
+      }
+      return getDots()
+    })
+};
+module.exports.getDots = getDots;
+module.exports.deleteDot = deleteDot;
+module.exports.addDot = addDot;
+module.exports.editDot = editDot;
